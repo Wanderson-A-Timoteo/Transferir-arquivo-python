5d6bac67cb2603b9a9e1382a44effbea2abd0da7
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2021-04-19 21:37:51 -0400

    Renomeado arquivo.txt para transferir.txt e inserido comentários no código

--- recebe_arquivo/cliente.py
+++ recebe_arquivo/cliente.py
@@ -1,14 +1,16 @@
-import socket
+import socket  ## Inportação do socket que irá permitir as conexões
 
-s = socket.socket()
-host = input(str("Digite o endereço de host do remetente :  "))
-port = 8080
-s.connect((host, port))
-print("Conectado...")
+s = socket.socket()  ## Atribui  "s" para o socket criar o objeto
+host = input(str("Digite o endereço de host do remetente :  ")) ## Aqui o usuario entra com uma string com o endereço do servidor
+port = 8080  ## Fornece a porta para conexão
+s.connect((host, port))  ## Conecta as portas do host fornecidas
+print("Conectado...")  ## Se estiver tudo ok com a conexão é imprime a msn Conectado...
 
-filename = input(str("Digite um nome de arquivo para o arquivo de entrada :  "))
-file = open(filename, 'wb')
+## CRIANDO O ARQUIVO RECEBIDO
+## Aqui criamos um nome qualquer para receber os dados do arquivo do servidor, O nome precisa ter o mesmo tipo por exemplo: txt 
+filename = input(str("Digite um nome para o arquivo de entrada :  "))
+file = open(filename, 'wb') ## Abre o arquivo no modo de escrita.
 file_data = s.recv(1024)
-file.write(file_data)
-file.close()
-print("O arquivo foi recebido com sucesso!")
+file.write(file_data)  ## Escreve os dados recebidos no arquivo.
+file.close()  ## Fecha o arquivo.
+print("O arquivo foi recebido com sucesso!")  ## Se a transferencia for bem sucedida, imprime a msn de sucesso.
--- servidor.py
+++ servidor.py
@@ -1,17 +1,19 @@
-import socket
+import socket  ## Inportação do socket que irá permitir as conexões
 
-s = socket.socket()
-host = socket.gethostname()
-port = 8080
-s.bind((host, port))
-s.listen(1)
-print(host)
-print("Aguardando qualquer conexão de entrada ...")
-conn, addr = s.accept()
-print(addr, "Se conectou ao servidor!")
+s = socket.socket() ## Atribui  "s" para o socket criar o objeto
+host = socket.gethostname() ## host recebe  o endereço do programa
+port = 8080  ## Definido a porta 8080, mas poderia ser qualquer outra que você queira.
+s.bind((host, port))  ## Ligação do host e da porta com o socket
+s.listen(1) ## Aqui ficamos ouvindo uma conexão de entrada
+print(host) ## Imprime o endereço do servidor, que será usado pelo cliente se conectar ao servidor
+print("Aguardando qualquer conexão de entrada ...")  ## Imprime a msn que aguarda uma conexão de entrada
+conn, addr = s.accept()  ## Aceita todas as conexões
+print(addr, "Se conectou ao servidor!") ## imprime o endereço do cliente que faz a conexão
 
-filename = input(str("Por favor, digite o nome do arquivo :  "))
-file = open(filename , 'rb')
+## CRIANDO O ARQUIVO PARA TRANSFERÊNCIA
+##Aqui o usuario entra com uma string informando o nome do arquivo que deseja e salva em filename
+filename = input(str("Por favor, digite o nome do arquivo txt que deseja receber :   ")) 
+file = open(filename , 'rb') ## Abre o arquivo no modo de leitura de bytes.
 file_data = file.read(1024)
-conn.send(file_data)
-print("Os dados foram transmitidos com sucesso!")
+conn.send(file_data) ## Diz para a conexão enviar os dados ao cliente
+print("Os dados foram transmitidos com sucesso!") ## Se os dados for transmitidos, imprime a msn de sucesso
